9a163870fcbfadf19517c040ba13b53f62dbcd95
DaomBae <da6970@gmail.com>
2023-03-31 17:29:39 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Header from "./components/Header";
+import Introduce from "./components/Introduce";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    // Container
+    <div className="bg-red-100 min-h-screen">
+      <Header/>
+      <main>
+        <Introduce/>
+        <Portfolio/> 
+        <Contact/>
+        </main>
+        <Footer/>
+    </div>
+  );
 }
 
 export default App;
